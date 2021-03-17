689e9208fa514da38427cd09943449e2bf56fb20
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-03-17 14:44:40 +0900

    Add within and distance for Sphere and Disk

--- src/Sphere.jl
+++ src/Sphere.jl
@@ -20,8 +20,68 @@ function moment_of_inertia(x::Sphere)
                           0 0 I]), :U)
 end
 
-distance(line::Line, x::Sphere) = distance(line, centroid(x), radius(x))
-distance(poly::Polygon, x::Sphere) = distance(poly, centroid(x), radius(x))
+"""
+    within(x::Vec, ::Sphere; include_bounds = true)
+
+Check if `x` is within a sphere.
+
+```jldoctest
+julia> sphere = Sphere(Vec(1.0,1.0), 1.0)
+1-element Sphere{2,Float64}:
+ [1.0, 1.0]
+
+julia> within(Vec(0.5, 0.5), sphere)
+true
+
+julia> within(Vec(0.0, 0.0), sphere)
+false
+```
+"""
+function within(x::Vec{dim}, sphere::Sphere{dim}; include_bounds::Bool = true) where {dim}
+    c = center(sphere)
+    d = (x - c)
+    d² = d ⋅ d
+    r² = radius(sphere)^2
+    d² == r² && return include_bounds
+    d² < r²
+end
+
+"""
+    distance(::Sphere, x::Vec)
+    distance(::Sphere, x::Vec, threshold::Real)
+
+Compute the distance vector from `x` to perpendicular foot on surface of sphere.
+When `threshold` is given, check the contact between line and point `x`,
+and return `nothing` if contact is not detected.
+
+```jldoctest
+julia> sphere = Sphere(Vec(1.0,1.0), 1.0)
+1-element Sphere{2,Float64}:
+ [1.0, 1.0]
+
+julia> d = distance(sphere, Vec(0.0,0.0))
+2-element Tensor{Tuple{2},Float64,1,2}:
+ 0.29289321881345254
+ 0.29289321881345254
+
+julia> norm(d) ≈ √2 - 1
+true
+```
+"""
+function distance(sphere::Sphere{dim}, x::Vec{dim}) where {dim}
+    v = center(sphere) - x
+    norm_v = norm(v)
+    d = norm_v - radius(sphere)
+    d * (v / norm_v)
+end
+
+function distance(sphere::Sphere{dim}, x::Vec{dim}, r::Real) where {dim}
+    v = center(sphere) - x
+    norm_v = norm(v)
+    d = norm_v - radius(sphere)
+    d - r ≤ 0 && return (d - r) * (v / norm_v)
+    nothing
+end
 
 
 mutable struct Disk{dim, T} <: GeometricObject{dim, T}
@@ -46,5 +106,7 @@ function moment_of_inertia(x::Disk{2})
                           0 0 r^2/2]), :U)
 end
 
-distance(line::Line, x::Disk) = distance(line, centroid(x), radius(x))
-distance(poly::Polygon, x::Disk) = distance(poly, centroid(x), radius(x))
+to_sphere(disk::Disk) = Sphere(coordinates(disk), radius(disk))
+within(disk::Disk, x::Vec{2}; include_bounds::Bool = true) = within(to_sphere(disk), x; include_bounds)
+distance(disk::Disk, x::Vec{2}) = distance(to_sphere(disk), x)
+distance(disk::Disk, x::Vec{2}, r::Real) = distance(to_sphere(disk), x, r)
