a3a99ec9e92da6cd676bbada7b562721d63e7184
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-03-22 14:39:08 +0900

    Fix `in` for `Circle`

--- src/Sphere.jl
+++ src/Sphere.jl
@@ -106,7 +106,7 @@ function moment_of_inertia(x::Circle{2})
                           0 0 r^2/2]), :U)
 end
 
-Sphere(circle::Circle) = Sphere(coordinates(circle), radius(circle))
-Base.in(circle::Circle, x::Vec{2}; include_bounds::Bool = true) = in(Sphere(circle), x; include_bounds)
+Sphere(circle::Circle) = Sphere(centroid(circle), radius(circle))
+Base.in(x::Vec{2}, circle::Circle; include_bounds::Bool = true) = in(x, Sphere(circle); include_bounds)
 distance(circle::Circle, x::Vec{2}) = distance(Sphere(circle), x)
 distance(circle::Circle, x::Vec{2}, r::Real) = distance(Sphere(circle), x, r)
