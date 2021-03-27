fd564af5430d57f1852c9314d3f4166eeb2af615
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-03-27 10:25:12 +0900

    Add vtk_grid for Sphere{2}

--- src/vtk.jl
+++ src/vtk.jl
@@ -24,3 +24,7 @@ function WriteVTK.vtk_grid(vtk::AbstractString, circle::Circle)
     cells = [MeshCell(VTKCellTypes.VTK_POLYGON, collect(1:size(coords, 2)))]
     vtk_grid(vtk, coords, cells)
 end
+
+function WriteVTK.vtk_grid(vtk::AbstractString, sphere::Sphere{2})
+    vtk_grid(vtk, Circle(centroid(sphere), radius(sphere)))
+end
