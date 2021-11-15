0a9a10bb4b5431e84a072a1013a3bb6fea8891ef
Keita Nakamura <keita.nakamura.1109@gmail.com>
2021-11-15 12:34:58 +0900

    Implement `intersect` for `Polyline`

--- src/Polyline.jl
+++ src/Polyline.jl
@@ -32,3 +32,27 @@ function distance(poly::Polyline{dim, T}, x::Vec{dim, T}, r::T; include_tips::Bo
     end
     nothing
 end
+
+# almost copied from `Polygon`, should be unified?
+"""
+    intersect(::Polyline, ::AbstractLine; [extended = false])
+
+Find the closest intersection point from line to polyline.
+Return `nothing` if not found.
+"""
+function Base.intersect(poly::Polyline{dim, T}, line::AbstractLine; extended::Bool = false) where {dim, T}
+    output = zero(Vec{dim, T})
+    dist = T(Inf)
+    @inbounds for i in 1:length(poly)-1
+        p = intersect(getline(poly, i), line; extended = (false, extended))
+        p === nothing && continue
+        v = line[1] - p
+        vv = v ⋅ v
+        if vv < dist
+            output = p
+            dist = vv
+        end
+    end
+    isinf(dist) && return nothing
+    output
+end
--- test/Polyline.jl
+++ test/Polyline.jl
@@ -10,4 +10,13 @@
     @test distance(poly, Vec(-1.0, 20.0), 2.0, include_tips = false) == nothing
     @test distance(poly, Vec(0.0, 21.0), 2.0, include_tips = true)  == poly[3] - Vec(0.0, 21.0)
     @test distance(poly, Vec(0.0, 21.0), 2.0, include_tips = false) == nothing
+
+    # intersect
+    poly = Polyline([Vec(0.0,1.0), Vec(1.0,0.0), Vec(1.0,1.0)])
+    line = Line(Vec(0.0,0.0), Vec(0.2,0.2));
+    @test intersect(poly, line, extended = true) ≈ [0.5,0.5]
+    @test intersect(poly, line, extended = false) === nothing
+    line = Line(Vec(2.0,-0.2), Vec(1.8,-0.2));
+    @test intersect(poly, line, extended = true) === nothing
+    @test intersect(poly, line, extended = false) === nothing
 end
