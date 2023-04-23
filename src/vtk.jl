1195d94a9cb134800a7d209e8e9362c51a05392a
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-04-23 20:11:03 +0900

    Refactor vtk.jl

--- src/vtk.jl
+++ src/vtk.jl
@@ -1,31 +1,31 @@
 function vtk_format(x::AbstractVector{Vec{dim, T}}) where {dim, T}
-    reshape(reinterpret(T, x), (dim, length(x)))
+    reinterpret(reshape, T, x)
 end
 function vtk_format(x::SVector{<: Any, Vec{dim, T}}) where {dim, T}
     reshape(vcat(x...), (dim, length(x)))
 end
 
-function WriteVTK.vtk_grid(vtk::AbstractString, line::Line)
+function WriteVTK.vtk_grid(vtk::AbstractString, line::Line; kwargs...)
     coords = vtk_format(coordinates(line))
-    cells = [MeshCell(VTKCellTypes.VTK_LINE, collect(1:size(coords, 2)))]
-    vtk_grid(vtk, coords, cells)
+    cells = [MeshCell(VTKCellTypes.VTK_LINE, 1:size(coords, 2))]
+    vtk_grid(vtk, coords, cells; kwargs...)
 end
 
-function WriteVTK.vtk_grid(vtk::AbstractString, poly::Polygon)
+function WriteVTK.vtk_grid(vtk::AbstractString, poly::Polygon; kwargs...)
     coords = vtk_format(coordinates(poly))
-    cells = [MeshCell(VTKCellTypes.VTK_POLYGON, collect(1:size(coords, 2)))]
-    vtk_grid(vtk, coords, cells)
+    cells = [MeshCell(VTKCellTypes.VTK_POLYGON, 1:size(coords, 2))]
+    vtk_grid(vtk, coords, cells; kwargs...)
 end
 
-function WriteVTK.vtk_grid(vtk::AbstractString, circle::Circle)
+function WriteVTK.vtk_grid(vtk::AbstractString, circle::Circle; kwargs...)
     θᵢ = LinRange(0, 2π, 360)
     coords = vtk_format([centroid(circle) + radius(circle) * Vec(cos(θ), sin(θ)) for θ in θᵢ])
-    cells = [MeshCell(VTKCellTypes.VTK_POLYGON, collect(1:size(coords, 2)))]
-    vtk_grid(vtk, coords, cells)
+    cells = [MeshCell(VTKCellTypes.VTK_POLYGON, 1:size(coords, 2))]
+    vtk_grid(vtk, coords, cells; kwargs...)
 end
 
-function WriteVTK.vtk_grid(vtk::AbstractString, sphere::Sphere{2})
-    vtk_grid(vtk, Circle(centroid(sphere), radius(sphere)))
+function WriteVTK.vtk_grid(vtk::AbstractString, sphere::Sphere{2}; kwargs...)
+    vtk_grid(vtk, Circle(centroid(sphere), radius(sphere)); kwargs...)
 end
 
-WriteVTK.vtk_grid(vtk::AbstractString, obj::GeometricObject) = vtk_grid(vtk, geometry(obj))
+WriteVTK.vtk_grid(vtk::AbstractString, obj::GeometricObject; kwargs...) = vtk_grid(vtk, geometry(obj); kwargs...)
