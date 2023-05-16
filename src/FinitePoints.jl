ddeb7f6eba963c9dbe448ff45899c655441ac457
Keita Nakamura <keita.nakamura.1109@gmail.com>
2023-05-16 16:33:13 +0900

    Faster centroid and moment_of_inertia for FinitePoints

--- src/FinitePoints.jl
+++ src/FinitePoints.jl
@@ -5,9 +5,18 @@ end
 
 FinitePoints(coords::AbstractVector) = Geometry(FinitePoints, coords)
 
-centroid(geo::FinitePoints) = mean(coordinates(geo))
+function centroid(geo::FinitePoints{dim, T}) where {dim, T}
+    xc = zero(Vec{dim, T})
+    @inbounds @simd for x in coordinates(geo)
+        xc += x
+    end
+    xc / num_coordinates(geo)
+end
 
-function moment_of_inertia(geo::FinitePoints)
-    coords = coordinates(geo)
-    sum(x -> (x⋅x)*I-symmetric(x⊗x,:U), coords) / length(coords)
+function moment_of_inertia(geo::FinitePoints{dim, T}) where {dim, T}
+    Ic = zero(SymmetricSecondOrderTensor{dim, T})
+    @inbounds @simd for x in coordinates(geo)
+        Ic += (x⋅x)*I - symmetric(x⊗x, :U)
+    end
+    Ic / num_coordinates(geo)
 end
